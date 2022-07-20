33ac8e98821c24e6d314785afe7664b82ada077d
Mojtaba <mojtaba.esk@gmail.com>
2022-07-20 10:23:08 +0200

    AppName added

--- contract/main_test.go
+++ contract/main_test.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"math/big"
 	"os"
-	"reflect"
 	"strings"
 	"testing"
 
-	"github.com/mojtaba-esk/evmos-smart-contract/contract"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/version"
+	evmoscfg "github.com/evmos/evmos/v6/cmd/config"
+	"github.com/mojtaba-esk/evmos-smart-contract/cmd"
 )
 
 const (
@@ -33,99 +34,16 @@ func init() {
 	if TestDataDir == "" {
 		panic(fmt.Errorf("dataDirFileContainerPath: the path is empty"))
 	}
+
+	config := sdk.GetConfig()
+	evmoscfg.SetBech32Prefixes(config)
+	version.Name = cmd.AppName
+	config.Seal()
 }
 
 // Disabling logs
 func TestMain(m *testing.M) {
 	log.SetOutput(ioutil.Discard)
+
 	os.Exit(m.Run())
-}
-
-func TestParseJsonParams(t *testing.T) {
-
-	tt := []struct {
-		name       string
-		input      string
-		wantErr    bool
-		wantOutput []interface{}
-	}{
-		{
-			name:       "Empty input",
-			input:      "",
-			wantErr:    false,
-			wantOutput: []interface{}{},
-		},
-		{
-			name:       "Empty parameters",
-			input:      `{"params":[]}`,
-			wantErr:    false,
-			wantOutput: []interface{}{},
-		},
-		{
-			name:       "Malformed Json parameters",
-			input:      `{"params":[12,]}`,
-			wantErr:    true,
-			wantOutput: nil,
-		},
-		{
-			name:    "Numeric parameters",
-			input:   `{"params":[0,1,2,3,4,5,6,7,8,9,10]}`,
-			wantErr: false,
-			wantOutput: []interface{}{
-				big.NewInt(0),
-				big.NewInt(1),
-				big.NewInt(2),
-				big.NewInt(3),
-				big.NewInt(4),
-				big.NewInt(5),
-				big.NewInt(6),
-				big.NewInt(7),
-				big.NewInt(8),
-				big.NewInt(9),
-				big.NewInt(10),
-			},
-		},
-		{
-			name:    "Float point numbers",
-			input:   `{"params":[0.5,1.2,2.000014,3.01111,4.999999]}`,
-			wantErr: false,
-			wantOutput: []interface{}{
-				big.NewInt(0),
-				big.NewInt(1),
-				big.NewInt(2),
-				big.NewInt(3),
-				big.NewInt(4),
-			},
-		},
-		{
-			name:    "Mix string, numeric params",
-			input:   `{"params":[0,1,2,3,"test","Ciao"]}`,
-			wantErr: false,
-			wantOutput: []interface{}{
-				big.NewInt(0),
-				big.NewInt(1),
-				big.NewInt(2),
-				big.NewInt(3),
-				"test",
-				"Ciao",
-			},
-		},
-	}
-
-	for _, tc := range tt {
-
-		t.Run(tc.name, func(t *testing.T) {
-
-			gotOutput, err := contract.ParseJsonParams(tc.input)
-
-			if (err != nil) != tc.wantErr {
-				t.Errorf("ParseJsonParams() error = %v, wantErr %v", err, tc.wantErr)
-				return
-			}
-
-			if !reflect.DeepEqual(gotOutput, tc.wantOutput) {
-				t.Errorf("ParseJsonParams() = %v, want %v", gotOutput, tc.wantOutput)
-			}
-		})
-	}
 }
--- main.go
+++ main.go
@@ -2,25 +2,16 @@ package main
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/version"
+	evmoscfg "github.com/evmos/evmos/v6/cmd/config"
 	"github.com/mojtaba-esk/evmos-smart-contract/cmd"
 )
 
-var (
-	Bech32Prefix         = "evmos"
-	Bech32PrefixAccAddr  = Bech32Prefix
-	Bech32PrefixAccPub   = Bech32Prefix + sdk.PrefixPublic
-	Bech32PrefixValAddr  = Bech32Prefix + sdk.PrefixValidator + sdk.PrefixOperator
-	Bech32PrefixValPub   = Bech32Prefix + sdk.PrefixValidator + sdk.PrefixOperator + sdk.PrefixPublic
-	Bech32PrefixConsAddr = Bech32Prefix + sdk.PrefixValidator + sdk.PrefixConsensus
-	Bech32PrefixConsPub  = Bech32Prefix + sdk.PrefixValidator + sdk.PrefixConsensus + sdk.PrefixPublic
-)
-
 func main() {
-	cfg := sdk.GetConfig()
-	cfg.SetBech32PrefixForAccount(Bech32PrefixAccAddr, Bech32PrefixAccPub)
-	cfg.SetBech32PrefixForValidator(Bech32PrefixValAddr, Bech32PrefixValPub)
-	cfg.SetBech32PrefixForConsensusNode(Bech32PrefixConsAddr, Bech32PrefixConsPub)
-	cfg.Seal()
+	config := sdk.GetConfig()
+	evmoscfg.SetBech32Prefixes(config)
+	version.Name = cmd.AppName
+	config.Seal()
 
 	cmd.Execute()
 }
